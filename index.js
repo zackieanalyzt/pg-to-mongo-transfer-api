032af33a7b081cd8b32c3d5497abcc027cf37d4a
zackieanalyzt <analyztman@gmail.com>
2025-05-21 11:55:52 +0700

    Update index.js
    Update index.js
    configuration port for new machine

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ app.get('/migrate', async (req, res) => {
 });
 
 // เริ่มเซิร์ฟเวอร์
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3003, () => {
+  console.log('Server running on port 3003');
 });
 
 // เรียกดูข้อมูลจาก MongoDB
